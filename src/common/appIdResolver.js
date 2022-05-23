0e8a499e23fbf3125f7f8a8b9ae32439ecbeb56d
mwtheus <mwtheus.sh@gmail.com>
2022-05-23 13:52:13 -0300

    appId

--- src/common/appIdResolver.js
+++ src/common/appIdResolver.js
@@ -1,12 +1,12 @@
-import { userAppId } from './userAppId';
+//import { userAppId } from './userAppId';
 
 const AppIdMap = Object.freeze({
-    'bot.binary.com': userAppId,
-    'bot.binary.me' : userAppId,
-    'binary.bot'    : userAppId,
-    //'bot.binary.com': '1169',
-    //'bot.binary.me' : '15438',
-    //'binary.bot'    : '15481',
+    //'bot.binary.com': userAppId,
+    //'bot.binary.me' : userAppId,
+    //'binary.bot'    : userAppId,
+    'bot.binary.com': '1169',
+    'bot.binary.me' : '15438',
+    'binary.bot'    : '15481',
 });
 
 export default AppIdMap;
